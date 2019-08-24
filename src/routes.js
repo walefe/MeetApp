2baea046bfccd3ed3229a1c67c3f9f72b37f89af
Walefe <walefedev@gmail.com>
2019-08-24 16:23:55 -0300

    Add conection database meetapp.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = Router();
 
-routes.get('/users', (req, res) => res.json({ message: 'Hello world.' }));
+routes.get('/users', async (req, res) => {
+  const user = await User.create({
+    name: 'Walefe',
+    email: 'walefe@rocketseat.com.br',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
